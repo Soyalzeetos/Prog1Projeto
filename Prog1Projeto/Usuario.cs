21c368e73b7d0a2e877d5b10dc0c13c28aeff510
Soyal <pablorodriguesfrutal@gmail.com>
2026-06-29 19:35:28 -0300

    Salvando oque fiz

--- Prog1Projeto/Usuario.cs
+++ Prog1Projeto/Usuario.cs
@@ -3,27 +3,33 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace Prog1Projeto
 {
-    internal class Usuario
+
+    public abstract class Pessoa
     {
-        public class Cliente
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public abstract DateTime CalcularPrazoDevolucao(DateTime dataEmprestimo);
+    }
+
+    public class Aluno : Pessoa
+    {
+        public override DateTime CalcularPrazoDevolucao(DateTime dataEmprestimo)
         {
-            public int Id { get; set; }
-            public string Nome { get; set; }
-            public string Email { get; set; }
-            public void PreencheCliente(MySqlDataReader resultado)
-            {
-                this.Id = Convert.ToInt32(resultado[0]);
-                this.Nome = resultado[1].ToString();
-                this.Email = resultado[2].ToString();
-            }
-            public void Mostrar()
-            {
-                Console.WriteLine(this.Id + "\t" + this.Nome + "\t" + this.Email);
-            }
-            // Métodos Incluir, Buscar, Apagar e Alterar serão implementados abaixo
+            return dataEmprestimo.AddDays(7);
+        }
+    }
+
+    public class Professor : Pessoa
+    {
+        public override DateTime CalcularPrazoDevolucao(DateTime dataEmprestimo)
+        {
+            return dataEmprestimo.AddDays(15);
         }
     }
 }
