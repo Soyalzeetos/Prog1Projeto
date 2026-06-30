712d3635c509e6e97a8183f54f0c6f60d5cefb15
Soyal <pablorodriguesfrutal@gmail.com>
2026-06-30 20:52:23 -0300

    Adiciona Livro.cs ao projeto e atualiza usings
    Inclui o arquivo Livro.cs na solução e no projeto. Adiciona a diretiva using Prog1Projeto; nos arquivos Emprestimo.cs, Livro.cs e Program.cs para garantir o correto uso do namespace.

--- Prog1Projeto/Emprestimo.cs
+++ Prog1Projeto/Emprestimo.cs
@@ -4,6 +4,7 @@ using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prog1Projeto;
 
 namespace Prog1Projeto
 {
--- Prog1Projeto/Livro.cs
+++ Prog1Projeto/Livro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prog1Projeto;
 
 namespace Prog1Projeto
 {
