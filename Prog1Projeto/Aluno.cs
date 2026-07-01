ec742835917cf4689fc8038f65c265bcfba329dc
Soyal <pablorodriguesfrutal@gmail.com>
2026-07-01 18:21:31 -0300

    Adiciona construtores à classe Aluno
    Inclui dois novos construtores em Aluno: um que recebe id, nome e email, repassando-os ao construtor da classe base Usuario, e outro construtor padrão que inicializa os valores com 0 e strings vazias.

--- Prog1Projeto/Aluno.cs
+++ Prog1Projeto/Aluno.cs
@@ -8,6 +8,12 @@ namespace Prog1Projeto
 {
     public class Aluno : Usuario
     {
+        public Aluno(int id, string nome, string email) : base(id, nome, email)
+        {
+        }
+        public Aluno() : base(0, "", "")
+        {
+        }
         public override DateTime CalcularPrazoDevolucao(DateTime dataEmprestimo)
         {
             return dataEmprestimo.AddDays(7);
